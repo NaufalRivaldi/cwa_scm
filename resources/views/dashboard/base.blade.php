8cdac460306ce193470642dc9cc648f266d53c65
NaufalRivaldi <naufal.rivaldi33@gmail.com>
2020-05-12 13:53:10 +0800

    Tambah format excel

--- resources/views/dashboard/base.blade.php
+++ resources/views/dashboard/base.blade.php
@@ -27,9 +27,9 @@
     <link rel="apple-touch-icon" sizes="152x152" href="{{ asset('assets/favicon/apple-icon-152x152.png') }}">
     <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('assets/favicon/apple-icon-180x180.png') }}">
     <link rel="icon" type="image/png" sizes="192x192" href="{{ asset('assets/favicon/android-icon-192x192.png') }}">
-    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('assets/favicon/favicon-32x32.png') }}">
-    <link rel="icon" type="image/png" sizes="96x96" href="{{ asset('assets/favicon/favicon-96x96.png') }}">
-    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('assets/favicon/favicon-16x16.png') }}">
+    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('img/logo/logo-min.png') }}">
+    <link rel="icon" type="image/png" sizes="96x96" href="{{ asset('img/logo/logo-min.png') }}">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('img/logo/logo-min.png') }}">
     <link rel="manifest" href="{{ asset('assets/favicon/manifest.json') }}">
     <meta name="msapplication-TileColor" content="#ffffff">
     <meta name="msapplication-TileImage" content="{{ asset('') }}assets/favicon/ms-icon-144x144.png">
